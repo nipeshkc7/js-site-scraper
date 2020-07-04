e5bdb5ae92616c5cf8d9b0b0aa4064eb9fc90ab2
Arpan Kc <nipeshkc7@gmail.com>
2020-07-04 21:37:47 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+if (process.env.NODE_ENV !== 'production') {
+  process.env.AWS_LAMBDA_FUNCTION_NAME = 'mock';
+}
+
+async function scrape(url) {
+  const chromeExecPath = await chrome.executablePath;
+  try {
+    browser = await puppeteer.launch({
+      args: chrome.args,
+      defaultViewport: chrome.defaultViewport,
+      executablePath: process.env.CHROME_BIN || (await chrome.executablePath),
+      headless: chrome.headless,
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const pageContent = await page.content();
+    await browser.close();
+    return pageContent;
+  } catch (error) {
+    console.log(`Error trying to scrape : ${url}, ${error}`);
+    throw error;
+  }
+}
+
+module.exports = {
+  scrape,
+};
